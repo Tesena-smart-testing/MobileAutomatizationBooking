04ac1c401332e0457ae1e2f64f7d094fce37d8ae
marina51533 <marina51533@gmail.com>
2023-09-20 16:36:05 +0300

    added assertions,improved 2 scripts

--- tests/specs/app.hotel.spec.js
+++ tests/specs/app.hotel.spec.js
@@ -1,10 +1,10 @@
 import OnboardingPage from "../screenobjects/components/OnboardingPage";
 import LoginPage from "../screenobjects/components/LoginPage";
 import HotelScreen from "../screenobjects/HotelScreen";
-import Calendar from "../screenobjects/components/Calendar";
-import assert from "assert";
 
-describe("should be able find a hotel successfully", async () => {
+
+
+describe("Searching for a hotel", async () => {
   beforeEach(async () => {
     await OnboardingPage.onboard();
     await LoginPage.login();
@@ -12,12 +12,19 @@ describe("should be able find a hotel successfully", async () => {
   it("should be able to find a hotel", async () => {
     // Navigate to the hotel screen
     await HotelScreen.navigateToHotelScreen();
+    // Assert that the field for searching is displayed
+    await expect(HotelScreen.enterDestinationFieldInner).toBeDisplayed();
     // Enter destination and select dates
     await HotelScreen.searchForHotel("Prague", "Select dates");
     // Apply room and guests selection
     await HotelScreen.chooseRoomAndGuests();
+    // Assert that the field has right name
+    const elementHasText = await $("~Prague");
+    await expect(elementHasText).toBeDisplayed();
     // Perform the hotel search
     await HotelScreen.tapOnSearchBtn();
+    // Assert that cards with hotels are displayed
+    await expect(HotelScreen.descriptionTaxOfHotel).toBeDisplayed()
     // Sort the results
     await HotelScreen.sortBtn.click();
   });
--- tests/specs/app.login.spec.js
+++ tests/specs/app.login.spec.js
@@ -1,18 +1,23 @@
-// const assert = require("chai").assert;
 import LoginScreen from "../screenobjects/LoginScreen";
 import OnboardingPage from "../screenobjects/components/OnboardingPage";
 
-describe("should be able login with Google successfully", async () => {
+describe("Login with Google", () => {
   beforeEach(async () => {
+    // Perform onboarding before each test
     await OnboardingPage.onboard();
   });
-  it("should be able to login successfully", async () => {
+
+  it("should be able to log in with Google", async () => {
+    // Perform login with Google
     await LoginScreen.tapOnGoogleBtn();
-    browser.pause(2000);
+
+    // Assert that the Google Name button is displayed
+    await expect(LoginScreen.googleNameBtn).toBeDisplayed();
+
+    // Perform further actions as needed, such as tapping on Google Name button
     await LoginScreen.tapOnGoogleNameBtn();
   });
 });
 
 // npm install chai --save-dev
 // npx wdio run ./wdio.conf.js
-
