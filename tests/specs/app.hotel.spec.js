1a441aa11e26fba217a7cf7cfa23d4dd94e7c1d8
marina51533 <marina51533@gmail.com>
2023-09-22 10:10:25 +0300

    Added horizontal gesture

--- tests/specs/app.hotel.spec.js
+++ tests/specs/app.hotel.spec.js
@@ -1,9 +1,7 @@
 import OnboardingPage from "../screenobjects/components/OnboardingPage";
 import LoginPage from "../screenobjects/components/LoginPage";
 import HotelScreen from "../screenobjects/HotelScreen";
-import Gestures from '../helpers/Gestures'
-
-
+import Gestures from "../helpers/Gestures";
 
 describe("Searching for a hotel", async () => {
   beforeEach(async () => {
@@ -11,7 +9,7 @@ describe("Searching for a hotel", async () => {
     await LoginPage.login();
   });
   it("should be able to find a hotel", async () => {
-    await browser.pause(4000)
+    await browser.pause(4000);
     //Swipe up to search element
     const swipeElement = await $(`//*[contains(@text, "Travel articles")]`);
     await Gestures.swipeUp(0.75);
@@ -30,10 +28,20 @@ describe("Searching for a hotel", async () => {
     // Assert that the field has right name
     const elementHasText = await $("~Prague");
     await expect(elementHasText).toBeDisplayed();
+
+    // Horizontal swipe to left - Car rental btn
+    // Define the initial and final coordinates
+    const initialLocation = { x: 706, y: 333 };
+    const finalX = initialLocation.x - 100; // Adjust this value as needed for the desired swipe distance
+    const finalY = initialLocation.y;
+
+    // Call the swipe function with the coordinates
+    await Gestures.swipe(initialLocation, { x: finalX, y: finalY });
+
     // Perform the hotel search
     await HotelScreen.tapOnSearchBtn();
     // Assert that cards with hotels are displayed
-    await expect(HotelScreen.descriptionTaxOfHotel).toBeDisplayed()
+    await expect(HotelScreen.descriptionTaxOfHotel).toBeDisplayed();
     // Sort the results
     await HotelScreen.sortBtn.click();
   });
